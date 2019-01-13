97ea3abe33438cc95648a5eafc124e2377db2844
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-01-13 16:10:33 +0700

    Add utils and entity

---
+++ src/clj/acemotion/users/entity.clj
@@ -0,0 +1,13 @@
+; (ns acemotion.users.entity
+;   :require
+;     [acemotion.db.core] :refer :all
+;     [acemotion.utils] :refer :all)
+
+; (defn create-seed-user []
+;   (create-users! {:id (uuid)
+;                   :first_name "Chakrit"
+;                   :last_name "Likitkhajorn"
+;                   :email "chakrit.lj@gmail.com"
+;                   :pass}))
+
+
---
+++ src/clj/acemotion/utils/utils.clj
@@ -0,0 +1,3 @@
+(ns acemotion.utils)
+
+(defn uuid [] (java.util.UUID/randomUUID)))
