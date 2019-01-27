00c1e40d2e24798d193c33941ae475c6b728d08e
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-01-27 12:45:28 +0700

    Stop refer all to compojure api

--- src/clj/acemotion/routes/services.clj
+++ src/clj/acemotion/routes/services.clj
@@ -1,9 +1,10 @@
 (ns acemotion.routes.services
   (:require [ring.util.http-response :as response]
-            [compojure.api.sweet :refer :all]
+            [compojure.api.sweet :as compojure-api]
             [schema.core :as s]
             [compojure.api.meta :refer [restructure-param]]
             [acemotion.core.users.services :as user-service]
+            [acemotion.core.alerts.routes :as alerts-routes]
             [buddy.auth.accessrules :refer [restrict]]
             [buddy.auth :refer [authenticated?]]))
 
@@ -27,13 +28,13 @@
    :response s/Any})
 
 (def service-routes
-  (api
+  (compojure-api/api
     {:swagger {:ui "/swagger-ui"
                :spec "/swagger.json"
                :data {:info {:version "1.0.0"
                              :title "Sample API"
                              :description "Sample Services"}}}}
-    (POST "/login" []
+    (compojure-api/POST "/login" []
       :return      api-response
       :body-params [username :- s/Str, password :- s/Str]
       :summary "Login and get authentication token"
@@ -43,14 +44,14 @@
            {:ok false :response "Error authentication"}
            {:ok true  :response {:token token}})}))
 
-    (context "/api" []
+    (compojure-api/context "/api" []
       :auth-rules authenticated?
       :header-params [authorization :- s/Str]
       :tags ["private"]
 
-      (GET "/sanity" []
+      (compojure-api/GET "/sanity" []
            (response/ok "I am sane"))
 
-      (GET "/whoami" []
+      (compojure-api/GET "/whoami" []
            :current-user user
            (response/ok user)))))
